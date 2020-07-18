be86a954465b795e8d9f8ca279becca09455ea02
Fabio Gavilondo <fgavilondo@icloud.com>
2020-07-18 21:51:01 +1000

    Ch 5 WIP

--- the-book/src/main.rs
+++ the-book/src/main.rs
@@ -1,7 +1,8 @@
 // Based on online book: The Rust Programming Language
 // https://doc.rust-lang.org/book/title-page.html
 
-// You declare constants using the const keyword instead of the let keyword, and the type of the value must be annotated:
+// You declare constants using the const keyword instead of the let keyword, and the type of the
+// value MUST be annotated:
 const CONST_FIVE: u32 = 5;
 
 fn ch3_common_concepts_variables() {
@@ -34,15 +35,15 @@ fn ch3_common_concepts_variables() {
     // immutable after those transformations have been completed.
     // Because we’re effectively creating a new variable when we use the let keyword again,
     // we can change the type of the value but reuse the same name:
-    let dashes = "---"; // String literals are slices! (see below)
+    let dashes = "---"; // String literals are slices, not proper strings! (see below)
     println!("dashes as &str: {}", dashes);
     let dashes = dashes.len();
     println!("shadowed dashes:  {}", dashes);
 
-    // Note: String not the same as &str
+    // Note: String is not the same as &str
     // str is an immutable sequence of UTF-8 bytes of dynamic length somewhere in memory.
     // Since the size is unknown, one can only handle it behind a pointer.
-    // This means that str most commonly2 appears as &str: a reference to some UTF-8 data,
+    // This means that str most commonly appears as &str: a reference to some UTF-8 data,
     // normally called a "string slice" or just a "slice".
 
     // String is the dynamic heap string type, like Vec: use it when you need to own or modify
@@ -59,7 +60,7 @@ fn ch3_common_concepts_variables() {
     let mut dashes: String = String::from("---");
     println!("dashes as String: {}", dashes);
     dashes.push('=');
-    println!("dashes modified: {}", dashes);
+    println!("dashes modified: {}", dashes); // ---=
     let str_slice = &dashes[1..];
     println!("slice 3..len: {}", str_slice);
     dashes.clear();
@@ -70,7 +71,7 @@ fn ch3_common_concepts_variables() {
 
     let a = [1, 2, 3, 4, 5];
     let arr_slice = &a[1..3];
-    println!("array slice: {:?}", arr_slice);
+    println!("array slice: {:?}", arr_slice); // [2, 3]
 }
 
 fn type_of<T>(_: &T) -> &'static str {
@@ -120,7 +121,7 @@ fn ch3_common_concepts_compound_data_types() {
     // A tuple is a general way of grouping together a number of values with a variety of types
     // into one compound type. Tuples have a fixed length:
     // once declared, they cannot grow or shrink in size.
-    let tup1 = (500, 6.4, 'z');
+    let tup1 = (100, 1.4, 'z');
     println!("tup1 = {:?}", tup1);
 
     // To get the individual values out of a tuple, we can use pattern matching to destructure it:
@@ -131,7 +132,7 @@ fn ch3_common_concepts_compound_data_types() {
     let tup1_1 = tup1.1;
     println!("tup1_1 = {}", tup1_1);
 
-    let tup2: (i32, f64, u8) = (500, 6.4, 1); // with explicit type annotation
+    let tup2: (i32, f64, u8) = (200, 2.4, 2); // with explicit type annotation
     println!("tup2 (pretty print) = {:#?}", tup2);
 
     println!();
@@ -156,7 +157,7 @@ fn ch3_common_concepts_compound_data_types() {
     println!("After each_plus_one() = {:?}", arr2); //  [4, 4, 4, 4, 4];
 
     println!();
-    println!("Vector (Note: not a Compound Data Type!)");
+    println!("Vector (Note: not a Compound data type!)");
 
     // Note: vectors are the heap equivalent of arrays, they can grow or shrink in size:
     let mut v = vec![1, 2, 3, 4, 5];
@@ -172,7 +173,7 @@ fn ch3_common_concepts_control_flow() {
 
     println!("if expressions:");
     let number = 12;
-    if number % 4 == 0 { // the good old modulo division trick
+    if number % 4 == 0 { // good old modulo division trick
         println!("{} is divisible by 4", number);
     } else if number % 3 == 0 {
         println!("{} is divisible by 3", number);
@@ -255,38 +256,35 @@ fn ch4_ownership_move_with_variables() {
     // Stack values implement the Copy trait.
 
     let i = 42;
-    let i_copy = i;
+    let mut i_copy = i;
+    i_copy += 1;
     println!("i = {}", i);
-    println!("i_copy = {}", i_copy);
+    println!("i_copy = {}", i_copy); // 43
 
     // However, for heap variables an assignment transfers ownership:
 
     let s = String::from("hello"); // s comes into scope
     let s2 = s; // s2 takes ownership of the value "hello"
-    // allocated heap memory will be freed when s2 goes out of scope (s is ignored from now on)
-    println!("s2 = {}", s2);
+
     // Move is like a "shallow copy", meaning we copy the pointer, the length, and the capacity
     // that are on the stack. We do not copy the data on the heap that the pointer refers to.
     // But once you move ownership, you cannot use the old variable anymore:
     // println!("s = {}", s); // compile error: "value borrowed here after move"
 
-    // use clone() for "deep copy"
-    let s2_clone = s2.clone();
-    println!("s2clone = {}", s2_clone);
-    // you can still use the cloned variable as the clone is a separate copy, not a move
+    // allocated heap memory will be freed when s2 goes out of scope (s is ignored from now on)
     println!("s2 = {}", s2);
+
+    // use clone() for "deep copy"
+    let s2_clone = s2.clone() + " world!";
+    println!("s2clone = {}", s2_clone);
+
+    // you can still use the cloned variable as the clone is a separate copy, not a move
+    println!("s2 still = {}", s2);
 }
 
 fn takes_ownership(some_string: String) { // some_string comes into scope
     println!("some_string = {}", some_string);
     // Here, some_string goes out of scope and `drop` is called. The backing memory is freed.
-}
-
-// takes_and_gives_back will take a String and return one
-fn takes_and_gives_back(a_string: String) -> String {
-    // a_string comes into scope
-    a_string
-    // a_string is returned and moves out to the calling function
 }
 
 fn makes_copy(some_integer: i32) { // some_integer comes into scope
@@ -301,6 +299,13 @@ fn gives_ownership() -> String {
     // s is returned and moves out to the calling function
 }
 
+// takes_and_gives_back will take a String and return one
+fn takes_and_gives_back(a_string: String) -> String {
+    // a_string comes into scope
+    a_string
+    // a_string is returned and moves out to the calling function
+}
+
 fn calculate_length_moved(s: String) -> (String, usize) {
     let length = s.len();
     (s, length)
@@ -312,7 +317,8 @@ fn ch4_ownership_move_with_functions() {
     println!();
 
     // The semantics for passing a value to a function are similar to those for assigning a value
-    // to a variable. Passing a variable to a function will move or copy, just as assignment does.
+    // to a variable. Passing a variable to a function will move (heap types) or copy (scalar types),
+    // just as assignment does.
 
     let s = String::from("hello"); // s comes into scope
 
@@ -329,13 +335,13 @@ fn ch4_ownership_move_with_functions() {
 
     // Returning values from functions can also transfer ownership:
 
-    let s2 = gives_ownership(); // gives_ownership moves its return value into s2
-    println!("s2 = {}", s2);
+    let s_that_came_from_a_function = gives_ownership(); // gives_ownership moves its return value into s2
+    println!("s_that_came_from_a_function = {}", s_that_came_from_a_function);
 
     let s3 = String::from("hello");    // s3 comes into scope
-    let s4 = takes_and_gives_back(s3);
+    let s_passed_through_a_function = takes_and_gives_back(s3);
     // s3 is moved into takes_and_gives_back(), which also moves its return value into s4
-    println!("s4 = {}", s4);
+    println!("s_passed_through_a_function = {}", s_passed_through_a_function);
     // println!("s3 = {}", s3); // value borrowed here after move
 
     // Taking ownership and then returning ownership with every function is a bit tedious.
@@ -345,7 +351,7 @@ fn ch4_ownership_move_with_functions() {
     // might want to return as well.
     // It’s possible to return multiple values using a tuple:
 
-    let (s5, len) = calculate_length_moved(s4);
+    let (s5, len) = calculate_length_moved(s_passed_through_a_function);
     println!("The length of '{}' is {}.", s5, len);
 }
 
@@ -356,7 +362,7 @@ fn calculate_length(s: &String) -> usize {
     // it refers to, nothing happens.
 }
 
-fn change_borrowed(some_string: &String) {
+fn try_to_change_borrowed_immutable(some_string: &String) {
     // Just as variables are immutable by default, so are references.
     // We’re not allowed to modify something we have a reference to.
     // Compile error: cannot borrow `*some_string` as mutable, as it is behind a `&` reference
@@ -377,7 +383,6 @@ fn ch4_ownership_borrow() {
     // * At any given time, you can have either one mutable reference or any number of immutable references.
     // * References must always be valid.
 
-
     // The issue with the calculate_length_moved() function is that we have to return the String to
     // the calling function so we can still use the String after the call to calculate_length,
     // because the String was moved into calculate_length.
@@ -390,11 +395,11 @@ fn ch4_ownership_borrow() {
     println!("The length of '{}' is {}.", hello_str, len);
 
     // Note: The opposite of referencing by using & is dereferencing, which is accomplished with
-    // the dereference operator, *. (Chapter 15)
+    // the dereference operator, * (Chapter 15)
 
     // We call having references as function parameters 'borrowing'.
     // So what happens if we try to modify something we’re borrowing? It doesn’t work!
-    change_borrowed(&hello_str);
+    try_to_change_borrowed_immutable(&hello_str);
 
     // We can fix the error with just a small tweak:
     let mut hello_mut = String::from("hello");
@@ -404,8 +409,8 @@ fn ch4_ownership_borrow() {
 
     // But mutable references have one big restriction: you can have only one mutable reference to
     // a particular piece of data in a particular scope. This code will fail:
-    let mut s = String::from("hello");
-    let r1 = &mut s;
+    let mut s_mut = String::from("hello");
+    let r1 = &mut s_mut;
     // let r2 = &mut s; // error[E0499]: cannot borrow `s` as mutable more than once at a time
     println!("r1 = {}", r1);
 
@@ -416,16 +421,16 @@ fn ch4_ownership_borrow() {
 
     // We can use curly brackets to create a new scope, allowing for multiple mutable references,
     // just not simultaneous ones:
-    let mut s2 = String::from("hello");
+    let mut s2_mut = String::from("hello");
     {
-        let r1 = &mut s2;
+        let r1 = &mut s2_mut;
         println!("r1 = {}", r1);
     } // r1 goes out of scope here, so we can make a new reference with no problems.
-    let r2 = &mut s2;
+    let r2 = &mut s2_mut;
     println!("r2 = {}", r2);
 
     // We also cannot have a mutable reference while we have an immutable one:
-    let mut s3 = String::from("hello");
+    let mut s3 = String::from("hello"); // warning: variable does not need to be mutable
     let r3 = &s3; // no problem
     let r4 = &s3; // no problem
     // Compile error[E0502]: cannot borrow `s3` as mutable because it is also borrowed as immutable
@@ -436,40 +441,107 @@ fn ch4_ownership_borrow() {
     // any borrow must last for less than the scope of the owner
 }
 
-#[derive(Debug)] // needed so we can print it out with {:#?}
+// Debug trait needed so we can print out values with {:?} and {:#?}
+#[derive(Debug)]
 struct User {
+    // Note: if we wanted to use &str instead of String it would require the use of lifetimes (Chapter 10).
+    // Lifetimes ensure that the data referenced by a struct is valid for as long as the struct is.
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
 }
 
-fn build_user_using_field_init_shorthands(email: String, username: String) -> User {
-    // possible because the parameter names and the struct field names are exactly the same
+fn build_user_using_field_init_shorthands(email: String, username: String, count: u64) -> User {
     User {
-        email,
-        username,
+        username, // possible because the parameter name and the struct field name are exactly the same
+        email, // possible because the parameter name and the struct field name are exactly the same
+        sign_in_count: count,
         active: true,
-        sign_in_count: 1,
     }
 }
 
 fn ch5_structs() {
     println!();
     println!("5. Structs");
+    println!();
 
+    // Structs and enums (discussed in Chapter 6) are the building blocks for creating new types in your
+    // program’s domain to take full advantage of Rust’s compile time type checking.
+
+    // create an instance of the struct
     let user1 = User {
+        // We don’t have to specify the fields in the same order in which we declared them in the struct.
         email: String::from("someone@example.com"),
         username: String::from("someusername1"),
         active: true,
         sign_in_count: 1,
     };
-    println!("user1: {:#?}", user1);
 
+    println!("user1: {:?}", user1);
+    println!("user1 (pretty): {:#?}", user1);
+
+    // create a mutable instance of the struct
     let mut user2 = build_user_using_field_init_shorthands(String::from("someone@example.com"),
-                                                           String::from("someusername2"));
-    user2.email = String::from("anotheremail@example.com"); // user2 must be mutable
+                                                           String::from("someusername2"), 2);
+    // To get a specific value from a struct, we can use dot notation
+    user2.email = String::from("anotheremail@example.com"); // instance must be mutable to modify fields
     println!("user2: {:#?}", user2);
+
+    // struct update syntax:
+    // create a new instance of a struct that uses most of an old instance’s values but changes some
+    let user3 = User {
+        username: String::from("someusername3"),
+        ..user1
+    };
+    println!("user3 (mostly copy of user1): {:#?}", user3);
+}
+
+fn ch5_tuple_structs() {
+    println!();
+    println!("5. Tuple Structs");
+    println!();
+
+    // You can also define structs that look similar to tuples, called tuple structs.
+    // Tuple structs have the added meaning the struct name provides but don’t have names associated with their fields;
+    // rather, they just have the types of the fields. Tuple structs are useful when you want to give the whole tuple
+    // a name and make the tuple be a different type from other tuples, and naming each field as in a regular struct
+    // would be verbose or redundant.
+
+    #[derive(Debug)]
+    struct Color(i32, i32, i32);
+
+    #[derive(Debug)]
+    struct Point(i32, i32, i32);
+
+    let black = Color(0, 0, 0);
+    println!("Color - black: {:?}", black);
+
+    let origin = Point(0, 1, 2);
+    println!("Point - origin: {:?}", origin);
+
+    // Otherwise, tuple struct instances behave like tuples...
+    // you can destructure them into their individual pieces:
+    let Color(r, g, b) = black;
+    println!("black destructured - r: {}, g: {}, b:{}", r, g, b);
+    // and you can use a . followed by the index to access an individual value:
+    println!("origin - x: {}", origin.0);
+}
+
+fn ch5_struct_newtypes() {
+    println!();
+    println!("5. Newtype Pattern");
+    println!();
+
+    // When a tuple struct has only one element, we call it newtype pattern. Because it helps to create a new type.
+
+    struct Kilometers(i32);
+
+    let distance = Kilometers(20);
+    // Destructure the instance using a `let` binding
+    let Kilometers(distance_in_km) = distance;
+    println!("The distance: {} km", distance_in_km); // The distance: 20 km
+    println!("Type of distance = {}", type_of(&distance));
 }
 
 fn main() {
@@ -486,5 +558,7 @@ fn main() {
     ch4_ownership_borrow();
 
     ch5_structs();
+    ch5_tuple_structs();
+    ch5_struct_newtypes();
 }
 
