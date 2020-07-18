0a2ea755033017320127d5bd9adc6a99c74ab943
Fabio Gavilondo <fgavilondo@icloud.com>
2020-07-18 23:01:25 +1000

    Add first classes

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,41 @@
+use chrono::{DateTime, Utc};
+
+#[derive(Debug)]
+struct Participant {
+    name: String,
+}
+
+impl Participant {
+    fn new(name: &str) -> Participant {
+        Participant {
+            name: String::from(name),
+        }
+    }
+
+    fn say(&self, what: &str) -> ChatMessage {
+        return ChatMessage::new(&self.name, what);
+    }
+}
+
+#[derive(Debug)]
+struct ChatMessage {
+    name: String,
+    text: String,
+    timestamp: DateTime<Utc>,
+}
+
+impl ChatMessage {
+    fn new(name: &str, text: &str) -> ChatMessage {
+        ChatMessage {
+            name: String::from(name),
+            text: String::from(text),
+            timestamp: Utc::now(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let steve = Participant::new("Steve");
+    let message = steve.say("Hello!");
+    println!("message: {:#?}", message);
 }
