31ade8384a5793d2653b888ea7c7e85e14e282d9
Fabio Gavilondo <fgavilondo@icloud.com>
2020-07-21 18:15:38 +1000

    WIP

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 use chrono::{DateTime, Utc};
 
 const NUMBER_OF_STUDENTS: u8 = 3;
-const NUMBER_OF_CONCEPTS: u8 = 10;
+const NUMBER_OF_CONCEPTS: u8 = 5;
 
 pub fn type_of<T>(_: &T) -> &'static str {
     std::any::type_name::<T>()
@@ -49,10 +49,11 @@ impl Student {
     }
 
     fn chat_away(&self) {
+        let tid = thread::current().id();
         for i in 1..NUMBER_OF_CONCEPTS + 1 {
             let message = self.chat(format!("Thing #{} understood", i).as_str());
-            println!("{:?}", message);
-            thread::sleep(Duration::from_millis(2200));
+            println!("{:?}: {:?}", tid, message);
+            thread::sleep(Duration::from_millis(1200));
         }
     }
 }
@@ -76,9 +77,11 @@ impl Presenter {
     }
 
     fn present(&self) {
+        let tid = thread::current().id();
         for i in 1..NUMBER_OF_CONCEPTS + 1 {
-            println!("Thing #{} that is great about Rust...", i);
-            thread::sleep(Duration::from_millis(2000));
+            println!();
+            println!("{:?}: Thing #{} that is great about Rust...", tid, i);
+            thread::sleep(Duration::from_millis(1000));
         }
     }
 }
@@ -99,16 +102,23 @@ fn main() {
     thread_handles.push(handle);
 
     for i in 1..NUMBER_OF_STUDENTS + 1 {
-        // use 'move' to take ownership of 'i' inside closure
+        // Use 'move' closure to be able to use variable 'i' (declared in main thread) in the spawned thread.
+        // It transfer ownership of values from one thread to another.
+        // This is necessary because Rust can’t tell how long the spawned thread will run, so it doesn’t know if the
+        // reference to i will always be valid.
         let handle: thread::JoinHandle<()> = thread::spawn(move || {
-            let student = Student::new(format!("{}{}", "Student_", i).as_str());
+            // i+2 to make student number match thread id
+            let student = Student::new(format!("{}{}", "Student_", i + 2).as_str());
             student.chat_away();
         });
         thread_handles.push(handle);
     }
 
-    // wait for all threads to finish
+    // block the main thread until all threads have finished
     for h in thread_handles {
         h.join().unwrap();
     }
+
+    println!();
+    println!("{:?}: End of training session", thread::current().id());
 }
